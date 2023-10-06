5d88365c15bda2e75dd53305cec2b9fd60b2a4ca
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-10-06 05:53:00 +0100

    update-swisscontract

---
+++ contracts/Swiss.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+//swisstronik-challenge
+contract Swiss {
+    //declaring the variable as private
+    string private bountyWinner;
+
+    /**
+     * @dev Constructor is used to set the initial message for the contract
+     * @param _winner: The Discord handle of the winner to be set as thr bountyWinner variable
+     */
+    constructor(string memory _winner) payable{
+        bountyWinner = _winner;
+    }
+
+    /**
+     * @dev setWInner() updates the stored winner in the contract
+     * @param _winner The new winner to replace the existing one
+     */
+    function setWinner(string memory _winner) public {
+        bountyWinner = _winner;
+    }
+
+    /**
+     * @dev getWinner() retrieves the currently stored winner in the contract
+     * @return The winner 
+     */
+    function getWinner() public view returns(string memory){
+        return bountyWinner;
+    }
+}
